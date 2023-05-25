c4a8dc07b9656f97b0ec509d10999a6b6274a136
amish <amishsxt147@gmail.com>
2023-05-25 23:05:02 +0530

    amish commit 2

--- app/src/main/java/com/example/positocabs/Views/EditProfileScreen.java
+++ app/src/main/java/com/example/positocabs/Views/EditProfileScreen.java
@@ -1,4 +1,159 @@
 package com.example.positocabs.Views;
 
-public class EditProfileScreen {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.positocabs.R;
+
+import java.util.Calendar;
+
+public class EditProfileScreen extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private EditText name,phoneNo,email,address;
+    private TextView dob;
+    private ImageView pfpEditBtn,backBtn;
+    private Spinner gender;
+    private AppCompatButton updateBtn;
+    private DatePickerDialog datePickerDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile_screen);
+
+        name=findViewById(R.id.name_edit_text);
+        phoneNo=findViewById(R.id.phone_no_edit_text);
+        email=findViewById(R.id.email_edit_text);
+        address=findViewById(R.id.address_edit_text);
+        dob=findViewById(R.id.dob_edit_text);
+        gender=findViewById(R.id.gender_spinner);
+        pfpEditBtn=findViewById(R.id.pfp_edit);
+        backBtn=findViewById(R.id.back_btn);
+        updateBtn=findViewById(R.id.update_btn);
+
+        //Gender logic (Spinner)
+
+        // Create an ArrayAdapter using a string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.gender_options, android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Apply the adapter to the spinner
+        gender.setAdapter(adapter);
+
+        gender.setOnItemSelectedListener(this);
+
+        //DOB logic
+        initDatePicker();
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePickerDialog.show();
+            }
+        });
+
+        //Update btn logic
+        updateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(EditProfileScreen.this, "updated!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void initDatePicker(){
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month = month + 1;
+                String date = makeDateString(day,month,year);
+                dob.setText(date);
+            }
+        };
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this,style,dateSetListener,year,month,day);
+    }
+
+    private String makeDateString(int day,int month,int year){
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month){
+        String monthAbbreviation;
+
+        switch (month) {
+            case 1:
+                monthAbbreviation = "JAN";
+                break;
+            case 2:
+                monthAbbreviation = "FEB";
+                break;
+            case 3:
+                monthAbbreviation = "MAR";
+                break;
+            case 4:
+                monthAbbreviation = "APR";
+                break;
+            case 5:
+                monthAbbreviation = "MAY";
+                break;
+            case 6:
+                monthAbbreviation = "JUN";
+                break;
+            case 7:
+                monthAbbreviation = "JUL";
+                break;
+            case 8:
+                monthAbbreviation = "AUG";
+                break;
+            case 9:
+                monthAbbreviation = "SEP";
+                break;
+            case 10:
+                monthAbbreviation = "OCT";
+                break;
+            case 11:
+                monthAbbreviation = "NOV";
+                break;
+            case 12:
+                monthAbbreviation = "DEC";
+                break;
+            default:
+                monthAbbreviation = "JAN";
+                break;
+        }
+
+        return monthAbbreviation;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
